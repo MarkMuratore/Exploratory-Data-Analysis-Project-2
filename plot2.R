880d1f83388d9940a7ad243ca94e4d394ce985e4
MarkMuratore <mark.j.muratore@gmail.com>
2015-03-18 21:18:15 -0400

    updated scripts
    included file path

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+setwd("~/Desktop/Coursera/Exploratory Data Analysis/Course Project 2")
+
 ### This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
